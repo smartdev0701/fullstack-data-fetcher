39edd3e53ed2261cc90f0f1f2d95b0f66407af14
smartdev0701 <it.smartdev0701@gmail.com>
2022-04-27 09:52:41 -0400

    Implemented the user authentication

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,5 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Signup = () => <div>Implement Me</div>
-
+const Signup = () => {
+    const [username, setUsername] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
+    return (
+        <div>
+            <form
+                action="#"
+                method="POST"
+                onSubmit={async (evt) => {
+                    evt.preventDefault()
+                    await fetch('http://localhost:4000/signup', {
+                        method: 'POST',
+                        body: JSON.stringify({ username, password }),
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        credentials: 'include',
+                    })
+                }}
+            >
+                <div>
+                    <label htmlFor="username">Username</label>
+                    <input
+                        name="username"
+                        value={username}
+                        onChange={(evt) => setUsername(evt.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input
+                        name="password"
+                        value={password}
+                        onChange={(evt) => setPassword(evt.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="c-password">Confirm Password</label>
+                    <input
+                        name="c-password"
+                        value={password}
+                        onChange={(evt) => setPassword(evt.target.value)}
+                    />
+                </div>
+                <button type="submit">Signup</button>
+            </form>
+        </div>
+    )
+}
 export default Signup

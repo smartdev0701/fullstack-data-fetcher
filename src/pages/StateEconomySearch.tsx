5feb3702eddb11a81699129cf725146f24b99f68
smartdev0701 <it.smartdev0701@gmail.com>
2022-04-26 22:46:23 -0400

    Implemented the domestic product and employment industries data fetching

--- src/pages/StateEconomySearch.tsx
+++ src/pages/StateEconomySearch.tsx
@@ -1,5 +1,198 @@
 import React from 'react'
+import DataTable from 'react-data-table-component'
+import useStateEconomy, { StateEconomyResult } from '../hooks/useStateEconomy'
 
-const StateEconomySearch = () => <div>Implement Me</div>
+const baseColumns = [
+    {
+        name: 'Name',
+        selector: (row: StateEconomyResult) => row.name,
+    },
+]
+
+const economyColumns = [
+    {
+        name: 'Total Dollar Amount',
+        selector: (row: StateEconomyResult) =>
+            row.tradeSummary?.totalDollarAmount,
+    },
+    {
+        name: 'Total Tons',
+        selector: (row: StateEconomyResult) => row.tradeSummary?.totalTons,
+    },
+    {
+        name: 'Top 5 States in terms of dollars',
+        cell: (row: StateEconomyResult) => {
+            return (
+                <div style={{ marginTop: '.2rem', marginBottom: '.2rem' }}>
+                    {row.tradeSummary?.statesByDollars
+                        .slice(0, 5)
+                        .map(({ name, amount }) => {
+                            return (
+                                <div>
+                                    {name}: {amount}
+                                </div>
+                            )
+                        })}
+                </div>
+            )
+        },
+    },
+    {
+        name: 'Top 5 States in terms of tons',
+        cell: (row: StateEconomyResult) => {
+            return (
+                <div style={{ marginTop: '.2rem', marginBottom: '.2rem' }}>
+                    {row.tradeSummary?.statesByTons
+                        .slice(0, 5)
+                        .map(({ name, amount }) => {
+                            return (
+                                <div>
+                                    {name}: {amount}
+                                </div>
+                            )
+                        })}
+                </div>
+            )
+        },
+    },
+]
+
+const productionColumns = [
+    {
+        name: 'Top 5 Industries by dollars',
+        cell: (row: StateEconomyResult) => {
+            return (
+                <div style={{ marginTop: '.2rem', marginBottom: '.2rem' }}>
+                    {row.productionSummary?.productionTypeByDollars
+                        .slice(0, 5)
+                        .map(({ name, amount }) => {
+                            return (
+                                <div>
+                                    {name}: {amount}
+                                </div>
+                            )
+                        })}
+                </div>
+            )
+        },
+    },
+    {
+        name: 'Top 5 industries by tons',
+        cell: (row: StateEconomyResult) => {
+            return (
+                <div style={{ marginTop: '.2rem', marginBottom: '.2rem' }}>
+                    {row.productionSummary?.productionTypeByTons
+                        .slice(0, 5)
+                        .map(({ name, amount }) => {
+                            return (
+                                <div>
+                                    {name}: {amount}
+                                </div>
+                            )
+                        })}
+                </div>
+            )
+        },
+    },
+]
+
+const employmentColumns = [
+    {
+        name: 'Top Industry by Average Salary',
+        selector: (row: StateEconomyResult) =>
+            row.employmentSummary?.topIndustryByAverageSalary.industry,
+    },
+    {
+        name: 'Top Industry by Employee Count',
+        selector: (row: StateEconomyResult) =>
+            row.employmentSummary?.topIndustryByAverageSalary.industry,
+    },
+]
+
+const StateEconomySearch = () => {
+    const {
+        results,
+        clearFilter,
+        search,
+        nameFilter,
+        setSummariesToFetch,
+        summariesToFetch,
+    } = useStateEconomy()
+
+    if (!results) {
+        return <div>'Loading state economy data...'</div>
+    }
+    return (
+        <div>
+            <div>
+                <label htmlFor="state">Search for a state</label>
+                <input
+                    value={nameFilter}
+                    onChange={(evt) => search(evt.target.value)}
+                    name="state"
+                    type="text"
+                />
+                <button onClick={clearFilter}>Clear Search</button>
+            </div>
+            <div>
+                <input
+                    checked={summariesToFetch?.employment}
+                    onChange={() => {
+                        if (summariesToFetch) {
+                            setSummariesToFetch({
+                                ...summariesToFetch,
+                                employment: !summariesToFetch.employment,
+                            })
+                        }
+                    }}
+                    type="checkbox"
+                    id="employment"
+                />
+                <label htmlFor="employment">Employment</label>
+                <input
+                    checked={summariesToFetch?.production}
+                    onChange={() => {
+                        if (summariesToFetch) {
+                            setSummariesToFetch({
+                                ...summariesToFetch,
+                                production: !summariesToFetch.production,
+                            })
+                        }
+                    }}
+                    type="checkbox"
+                    id="production"
+                />
+                <label htmlFor="production">Production</label>
+                <input
+                    checked={summariesToFetch?.trade}
+                    onChange={() => {
+                        if (summariesToFetch) {
+                            setSummariesToFetch({
+                                ...summariesToFetch,
+                                trade: !summariesToFetch.trade,
+                            })
+                        }
+                    }}
+                    type="checkbox"
+                    id="trade"
+                />
+                <label htmlFor="trade">Trade</label>
+            </div>
+            <DataTable
+                title="State List"
+                // @ts-ignore
+                columns={[
+                    ...baseColumns,
+                    ...economyColumns,
+                    ...productionColumns,
+                    ...employmentColumns,
+                ]}
+                data={results}
+                progressPending={results === undefined}
+                pagination
+            />
+        </div>
+    )
+}
 
 export default StateEconomySearch
--- src/server/index.ts
+++ src/server/index.ts
@@ -4,9 +4,48 @@ import express from 'express'
 import http from 'http'
 import fetch from 'node-fetch'
 import { createSchema } from './db'
+import { maxBy, sortBy, reverse } from 'lodash'
+
+let states: State[] | null = null
 
 const typeDefs = gql`
     # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+    type InterstateTradeForState {
+        name: String
+        amount: Float
+    }
+
+    type InterstateTradeSummary {
+        name: String
+        totalDollarAmount: Float
+        totalTons: Float
+        statesByDollars: [InterstateTradeForState]
+        statesByTons: [InterstateTradeForState]
+    }
+
+    type IndustryByEmployeeSummary {
+        industry: String
+        employedCount: String
+    }
+
+    type IndustryByAverageSalarySummary {
+        industry: String
+        averageSalary: String
+    }
+
+    type EmploymentSummary {
+        topIndustryByEmployee: IndustryByEmployeeSummary
+        topIndustryByAverageSalary: IndustryByAverageSalarySummary
+    }
+
+    type ProductionSummary {
+        name: String
+        totalDollarAmount: Float
+        totalTons: Float
+        productionTypeByDollars: [InterstateTradeForState]
+        productionTypeByTons: [InterstateTradeForState]
+    }
 
     # State type
     type State {
@@ -14,6 +53,9 @@ const typeDefs = gql`
         key: String
         name: String
         slug: String
+        tradeSummary: InterstateTradeSummary
+        employmentSummary: EmploymentSummary
+        productionSummary: ProductionSummary
     }
 
     type Query {
@@ -21,20 +63,189 @@ const typeDefs = gql`
     }
 `
 
+interface State {
+    id: string
+    key: string
+    name: string
+    slug: string
+}
+
+interface InterstateTradeSummary {
+    'Millions Of Dollars': number
+    'Thousands Of Tons': number
+    Origin: string
+    'ID Origin': string
+    'Destination State': string // the description of what is being produced
+    'ID Destination State': string
+}
+
+interface DataUsaEmploymentResult {
+    'Industry Group': string
+    'Total Population': number
+    'Average Wage': number
+    Geography: string
+}
+
+interface DataUsaProductionResult {
+    SCTG2: string
+    'Millions Of Dollars': number
+    'Thousands Of Tons': number
+    Origin: 'Virginia'
+}
+
+interface EmploymentSummary {
+    topIndustryByEmployee: { industry: string; employedCount: number }
+    topIndustryByAverageSalary: { industry: string; averageSalary: number }
+}
+
+interface InterstateTradeStateResult {
+    name: string
+    totalDollarAmount: number
+    totalTons: number
+    statesByDollars: { name: string; amount: number }[]
+    statesByTons: { name: string; amount: number }[]
+}
+
+interface ProductionSummary {
+    name: string
+    totalDollarAmount: number
+    totalTons: number
+    productionTypeByDollars: { name: string; amount: number }[]
+    productionTypeByTons: { name: string; amount: number }[]
+}
+
 const resolvers = {
+    State: {
+        tradeSummary: async (parent: State) => {
+            const { id } = parent
+            const response = await fetch(
+                `https://datausa.io/api/data?Origin%20State=${id}&measure=Millions%20Of%20Dollars,Thousands%20Of%20Tons&drilldowns=Destination%20State&year=latest`
+            )
+            const data: any = await response.json()
+            const results: InterstateTradeSummary[] = data.data
+            const totalTons = results.reduce(
+                (totalTons, result) =>
+                    (totalTons += result['Thousands Of Tons']),
+                0
+            )
+            const totalDollarAmount = results.reduce(
+                (totalTons, result) =>
+                    (totalTons += result['Millions Of Dollars']),
+                0
+            )
+            const statesByDollars = reverse(sortBy(
+                results.map((result) => {
+                    return {
+                        name: result['Destination State'],
+                        amount: result['Millions Of Dollars'],
+                    }
+                }),
+                'amount'
+            ))
+            const statesByTons = reverse(sortBy(
+                results.map((result) => {
+                    return {
+                        name: result['Destination State'],
+                        amount: result['Thousands Of Tons'],
+                    }
+                }),
+                'amount'
+            ))
+            
+            return {
+                name: parent.name,
+                totalTons,
+                totalDollarAmount,
+                statesByDollars,
+                statesByTons,
+            } as InterstateTradeStateResult
+        },
+        employmentSummary: async (parent: State) => {
+            const { id } = parent
+            const response = await fetch(
+                `https://datausa.io/api/data?Geography=${id}&measure=Total%20Population,Average%20Wage&drilldowns=Industry%20Group&Year=latest`
+            )
+            const data: any = await response.json()
+            const results: DataUsaEmploymentResult[] = data.data
+            const maxByEmployee = maxBy(results, (result) => {
+                return result['Total Population']
+            })
+            const maxBySalary = maxBy(results, (result) => {
+                return result['Average Wage']
+            })
+
+            return {
+                topIndustryByAverageSalary: {
+                    industry: maxBySalary?.['Industry Group'],
+                    averageSalary: maxBySalary?.['Average Wage'],
+                },
+                topIndustryByEmployee: {
+                    industry: maxByEmployee?.['Industry Group'],
+                    employedCount: maxByEmployee?.['Total Population'],
+                },
+            } as EmploymentSummary
+        },
+        productionSummary: async (parent: State) => {
+            const { id } = parent
+            const response = await fetch(
+                `https://datausa.io/api/data?Origin%20State=${id}&measure=Millions%20Of%20Dollars,Thousands%20Of%20Tons&drilldowns=SCTG2&year=latest`
+            )
+            const data: any = await response.json()
+            const results: DataUsaProductionResult[] = data.data
+            const totalTons = results.reduce(
+                (totalTons, result) =>
+                    (totalTons += result['Thousands Of Tons']),
+                0
+            )
+            const totalDollarAmount = results.reduce(
+                (totalTons, result) =>
+                    (totalTons += result['Millions Of Dollars']),
+                0
+            )
+            const productionTypeByDollars = results.map((result) => {
+                return {
+                    name: result.SCTG2,
+                    amount: result['Millions Of Dollars'],
+                }
+            })
+            const productionTypeByTons = results.map((result) => {
+                return {
+                    name: result.SCTG2,
+                    amount: result['Thousands Of Tons'],
+                }
+            })
+            return {
+                name: parent.name,
+                totalTons,
+                totalDollarAmount,
+                productionTypeByDollars,
+                productionTypeByTons,
+            } as ProductionSummary
+        },
+    },
     Query: {
         states: async (parent: unknown, args: { name?: string }) => {
             const { name } = args
-            const response = await fetch(
-                'https://topaz.datausa.io/api/searchLegacy?dimension=Geography&hierarchy=State&limit=50000'
-            )
-            const data: any = await response.json()
-            if (name) {
-                return data.results.filter((state: { name: string }) =>
-                    state.name.toLowerCase().startsWith(name.toLowerCase())
+            if (states) {
+                if (name) {
+                    return states.filter((state: { name: string }) =>
+                        state.name.toLowerCase().startsWith(name.toLowerCase())
+                    )
+                }
+                return states
+            } else {
+                const response = await fetch(
+                    'https://topaz.datausa.io/api/searchLegacy?dimension=Geography&hierarchy=State&limit=50000'
                 )
+                const data: any = await response.json()
+                if (name) {
+                    return data.results.filter((state: { name: string }) =>
+                        state.name.toLowerCase().startsWith(name.toLowerCase())
+                    )
+                }
+                states = data.results
+                return data.results
             }
-            return data.results
         },
     },
 }
